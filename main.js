07db91cdfecc8a70445077362445d1fb5f0b74aa
LilyNaibaho <lilyanastasianaibaho@gmail.com>
2024-09-19 23:22:25 +0700

    Belajar expression, statement,comment dan variable

--- main.js
+++ main.js
@@ -1,3 +1,41 @@
+//Statement and Expresion
+
 const age=26;
 const nama='Lily Naibaho'
 console.log(`Namaku ${nama}, umurku ${age} Tahun`);
+// age dan nama dalam javascript disebut dengan statement (age and nama on javascript are called as "Statement")
+//sedangkan 26 and Lily Naibaho disebut dengan Expression/Value on a statement 
+//(Meanwhile, 26 and Lily Naibaho are called as "Expression on Javascript")
+
+
+/*Sekarang kita akan bahas tentang Comment
+ini adalah project Js Lily Naibaho
+Tanggal 19 September 2024 
+(Whatever your decisions are, you know yourself better. So keep going :) )
+*/
+console.log('Hai Readers');
+console.log('Hai Javascript');
+//console.log('Hai Lily');
+const PI=3.14;
+console.log(PI);
+console.log('Hai Lily Naibaho, terima kasih ya sudah mau belajar, i am proud of you')
+
+
+/* Variable
+Variable adalah tempat menampung suatu nilai, nilainya bisa berupa apa saja
+baik berupa teks, angka dan lain sebagainya. 
+Cara menuliskan variable dalam javascript ada 2 cara yaitu dengan sintaks "const" atau "let"
+
+Apa bedanya const dan let????
+
+Dalam javascript , kamu tidak bisa mengubah value/menginisialisasi ulang value dari sebuah variable
+jika variable tsb di buat dengan sintaks "const"
+sedangkan dengan "let" , kamu bisa menginisialisasi ulang nilai dari sebuah variable
+contoh:
+*/
+const username ='Lily Naibaho';
+let email ='lilyanastasia75@gmail.com';
+email='lilynaibaho@gmail.com'
+//username='Lily' => ini tidak boleh diinisialisasi ulang ya valuenya, karena variablenya dibuat dengan sintasks "const"
+console.log(username);
+console.log(email);
