e4513d7bca26c059a4678d6ed84aea8653dcd89a
LilyNaibaho <lilyanastasianaibaho@gmail.com>
2024-09-20 23:03:30 +0700

    Aturan penamaan variable di Javascript

--- main.js
+++ main.js
@@ -39,3 +39,53 @@ email='lilynaibaho@gmail.com'
 //username='Lily' => ini tidak boleh diinisialisasi ulang ya valuenya, karena variablenya dibuat dengan sintasks "const"
 console.log(username);
 console.log(email);
+
+
+/*Aturan penamaan Variable
+1. Tidak boleh menggunakan nama variable yang sama dalam satu function
+Contoh:*/
+//Get data employee
+const name='Lily';
+const umur='26';
+//Get data Device
+//const name='Macboock M2';  ->>>> const name='Macboock M2'; SyntaxError: Identifier 'name' has already been declared
+const year='2021';
+
+/*Jika ingin menggunakan variable dengan nama yang sama , maka tempatkan variable tersebut pada dua function yang berbeda
+Contoh: */
+
+function printEmployeeData(){
+    const name='Lily Naibaho';
+    const umur=26
+    
+    console.log('Nama saya: ',name);
+    console.log('Umur saya: ',umur);
+}
+
+function printDeviceData(){
+
+    const name='Macboock M2';
+    const year=2022;
+
+    console.log('Device yang saya gunakan untuk bekerja: ',name);
+    console.log('Keluaran tahun: ',year);
+}
+printEmployeeData();
+printDeviceData();
+
+/* 2. Untuk menuliskan nama variable, variable hanya boleh terdiri dari angka,huruf, underscore, dan dollar ($) saja*/
+//Contoh yang benar
+const firstName='Lily';
+const last_name='Naibaho';
+const $message='Semangat belajarnya anak baik';
+const userId1=123;
+
+
+//Contoh yang salah
+//const 1userid='123'; //SyntaxError: Invalid or unexpected token
+//const @message='Jangan Menyerah'; //SyntaxError: Invalid or unexpected token
+//const last name='Naibaho'; //SyntaxError: Invalid or unexpected token
+//const tinggi-badan='163 cm';//SyntaxError: Invalid or unexpected token
+
+//3. Nama variable tidak boleh diawali dengan angka
+//const 1userid='123' //SyntaxError: Invalid or unexpected token
